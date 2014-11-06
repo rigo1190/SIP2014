aa9c78c2264a3b402a29e4469ecd7a204b7e6d20
josihovo <josihovo@gmail.com>
2014-11-06 13:23:47 -0600

    Se agrega clases

---
+++ DataAccessLayer/Models/TechoFinancieroBitacora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class TechoFinancieroBitacora:Generica
+    {
+
+        public TechoFinancieroBitacora(){
+            this.detalleMovimientos = new HashSet<TechoFinancieroBitacoraMovimientos>();
+        }
+
+        public int EjercicioId { get; set; }
+        public int Movimiento { get; set; }
+        public  EnumTipoMovimientoTechoFinanciero  Tipo { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public string Oficio { get; set; }
+        public string Observaciones { get; set; }
+
+        public virtual Ejercicio Ejercicio { get; set; }
+
+        public virtual ICollection<TechoFinancieroBitacoraMovimientos> detalleMovimientos { get; set; }
+
+    }
+
+
+    public enum EnumTipoMovimientoTechoFinanciero
+    {
+        CargaInicial = 0, Transferencia = 1
+    }
+}
---
+++ DataAccessLayer/Models/TechoFinancieroBitacoraMovimientos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class TechoFinancieroBitacoraMovimientos:Generica
+    {
+        public int TechoFinancieroBitacoraId { get; set; }
+
+        public int TechoFinancieroId { get; set; }
+
+        public int Submovimiento { get; set; }
+
+        public int UnidadPresupuestalId { get; set; }
+
+        public decimal Incremento { get; set; }
+        
+        public decimal Decremento { get; set; }
+
+        public virtual TechoFinancieroBitacora TechoFinancieroBitacora { get; set; }
+        public virtual TechoFinanciero TechoFinanciero { get; set; }
+        public virtual UnidadPresupuestal UnidadPresupuestal { get; set; }
+
+    }
+}

8eda39cb82166daf3192d7f62ffab42fd5db143f
rigo1190 <rigo1190@gmail.com>
2014-11-07 13:10:58 -0600

    Se agrega clase para almacenar conocimiento

---
+++ SIP/NotasFunciones.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIP
+{
+    public class NotasFunciones
+    {
+
+        /*ARCHIVO QUE ALMACENA FUNCIONALIDADES IMPORTANTES
+         
+         */
+
+        #region FUNCIONES BASICAS
+
+
+        /* NOMBRE FUNCIONALIDAD                                                         UBICACION                                           DESCRIPCION BREVE
+         * 
+         * Subir Archivo                                                 Formas/Poa/EvaluacionPoa.aspx                         se realizo una funcion que permite subir un archivo de x formato
+         * 
+         * CARGA DE COMBOS ESCALONADOS                                   Formas/Poa/POA.aspx                                   Se realizó carga de combos escalonados con ajax toolkit. 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        #endregion
+
+        #region FUNCIONES LINQ
+
+        /* NOMBRE FUNCIONALIDAD                                                         UBICACION                                           DESCRIPCION BREVE
+         * 
+         *
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+
+        #endregion
+
+
+
+    }
+}

2208600dabdab200c6a8cb8961ae029b9f02f9d6
manoel <manoelruizmorales@gmail.com>
2014-11-12 20:28:33 -0600

    Agregar clase TipoFondo para definir el ambito del Fondo

---
+++ DataAccessLayer/Models/TipoFondo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Models
+{
+    public class TipoFondo:Generica
+    {
+        [Index(IsUnique = true)]
+        [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
+        public string Clave { get; set; }
+
+        [StringLength(100, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Index(IsUnique = true)]
+        public int Orden { get; set; }
+    }
+}

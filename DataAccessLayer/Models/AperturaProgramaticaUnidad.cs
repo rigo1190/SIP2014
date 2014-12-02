984696e59d72b9e96dbadebde5a4dcb7bcd9fb1c
manoel <manoelruizmorales@gmail.com>
2014-12-02 14:42:21 -0600

    Agregar campo plural para clase AperturaProgramaticaUnidad

--- DataAccessLayer/Models/AperturaProgramaticaUnidad.cs
+++ DataAccessLayer/Models/AperturaProgramaticaUnidad.cs
@@ -18,6 +18,9 @@ namespace DataAccessLayer.Models
         [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
         public string Nombre { get; set; }
 
+        [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
+        public string NombrePlural { get; set; }
+
         [Index(IsUnique = true)]
         public int Orden { get; set; }
     }

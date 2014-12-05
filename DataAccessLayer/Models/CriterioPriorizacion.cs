108a5bd22df6f3a5f133e653bc71f2d88312f38a
manoel <manoelruizmorales@gmail.com>
2014-12-05 14:03:03 -0600

    Agregar atributo AplicaConvenio en clase CriterioPriorizacion

--- DataAccessLayer/Models/CriterioPriorizacion.cs
+++ DataAccessLayer/Models/CriterioPriorizacion.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer.Models
 {
@@ -19,5 +14,6 @@ namespace DataAccessLayer.Models
 
         [Index(IsUnique = true)]
         public int Orden { get; set; }
+        public bool AplicaConvenio { get; set; }
     }
 }

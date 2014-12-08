494cc72e94ec0f6b71fef21d3a3e7fdd37083bda
manoel <manoelruizmorales@gmail.com>
2014-12-08 11:44:31 -0600

    Agregar atributo TipoLocalidad en la clase Localidad

--- DataAccessLayer/Models/Localidad.cs
+++ DataAccessLayer/Models/Localidad.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer.Models
 {
     public class Localidad:Generica
     {       
-        [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
-        [DefaultValue("L000")]
+        [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]       
         public string Clave { get; set; }
+
         [StringLength(100, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
         public string Nombre { get; set; }
         public decimal Latitud { get; set; }
@@ -22,8 +16,14 @@ namespace DataAccessLayer.Models
         public int PoblacionMasculina { get; set; }
         public int PoblacionFemenina { get; set; }
         public int PoblacionTotal { get; set; }
+
+        [Index("IX_Orden_MunicipioId", 1, IsUnique = true)]
         public int MunicipioId { get; set; }
-        public int? Orden { get; set; }
+
+        [Index("IX_Orden_MunicipioId", 2)]
+        public int Orden { get; set; }
+        public int TipoLocalidadId { get; set; }
+        public virtual TipoLocalidad TipoLocalidad { get; set; }
         public virtual Municipio Municipio { get; set; }
     }
 }

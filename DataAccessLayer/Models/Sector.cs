b0e430d9c670ccb387dbc8eebc6ea957a68b1a29
manoel <manoelruizmorales@gmail.com>
2015-01-30 17:03:16 -0600

    Agregar clase Sector

---
+++ DataAccessLayer/Models/Sector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Models
+{
+    public class Sector:Generica
+    {
+        public Sector() 
+        {
+            this.DetalleUnidadesPresupuestales = new HashSet<UnidadPresupuestal>();
+        }
+
+        [Index(IsUnique=true)]
+        [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
+        public string Clave { get; set; }
+
+        [StringLength(250, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Index(IsUnique = true)]
+        public int Orden { get; set; }
+        public virtual ICollection<UnidadPresupuestal> DetalleUnidadesPresupuestales { get; set; }
+    }
+}

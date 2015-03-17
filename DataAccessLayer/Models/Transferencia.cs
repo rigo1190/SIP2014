55b4e5da6248f06b367adef981e3af64e57b83f3
manoel <manoelruizmorales@gmail.com>
2015-03-17 17:28:38 -0600

    Agregar clase Transferencia para modulo de Transferencias

---
+++ DataAccessLayer/Models/Transferencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Models
+{
+    public class Transferencia:Generica
+    {
+        public Transferencia() 
+        {
+            this.Detalle = new HashSet<TransferenciaDetalle>();
+        }
+
+        [Index("IX_Numero",IsUnique=true)]
+        public int Numero { get; set; }
+        public DateTime FechaElaboracion { get; set; }
+        public int TechoFinancieroUnidadPresupuestalId { get; set; }
+        public virtual TechoFinancieroUnidadPresupuestal TechoFinancieroUnidadPresupuestal { get; set; }
+        public virtual ICollection<TransferenciaDetalle> Detalle { get; set; }
+    }
+}

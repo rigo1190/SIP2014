17c84f3336a682aad3a8a9aab403732081f5a9d4
manoel <manoelruizmorales@gmail.com>
2015-03-17 17:34:01 -0600

    Agregar clase TransferenciaDetalle para modulo de Transferencias

---
+++ DataAccessLayer/Models/TransferenciaDetalle.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DataAccessLayer.Models
+{
+    public class TransferenciaDetalle:Generica
+    {
+        [Index("IX_TransferenciaId_ObraId", IsUnique = true)]
+        public int TransferenciaId { get; set; }
+
+        [Index("IX_TransferenciaId_ObraId")]
+        public int ObraId { get; set; }
+        public decimal InversionEstimada { get; set; }
+        public decimal Reduccion { get; set; }
+        public decimal Ampliacion { get; set; }
+        public decimal InversionModificada { get; set; }
+        public string Observaciones { get; set; }
+        public virtual Transferencia Transferencia { get; set; }
+        public virtual Obra Obra { get; set; }
+
+    }
+   
+}
